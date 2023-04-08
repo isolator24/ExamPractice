eed6498275ff81cc87476008e087cd50419583f2
Anthony Wong <isolator24@gmail.com>
2023-04-08 20:11:49 +0800

    Solve two sum problem

---
+++ TwoSum.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExamPractice
+{
+    sealed internal class TwoSum
+    {
+        public TwoSum() { 
+        }
+        
+        public void solveTwoSumProblem()
+        {
+            var num = new int[] { 2, 7, 11, 15 };
+            var target = 9;
+            for (int i = 0; i < num.Length; i++)
+            {
+                for (int j = i + 1; j < num.Length; j++)
+                {
+                    if (num[j] == target - num[i])
+                    {
+                        Console.WriteLine(new int[] { i, j });
+                        Console.WriteLine($"index i is {i} with value of {num[i]} \r\nindex j is {j} with value of {num[j]} ");
+                    }
+
+                }
+            }
+        }
+    }
+}
